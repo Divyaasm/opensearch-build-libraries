c6ab129fa5b14067a10f79c2434f28d994bb67e6
Divya Madala <divyaasm@amazon.com>
2023-02-23 13:44:06 -0800

    validate-artifacts
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- vars/validateArtifacts.groovy
+++ vars/validateArtifacts.groovy
@@ -27,8 +27,10 @@ void call(Map args = [:]) {
                 workdir = "${WORKSPACE}/opensearch-build"
             }
         }
+        echo '${args.version}'
+        println this.args[0]
 
         sh """
-                   ${workdir}/validation.sh ${args}
+                   ${workdir}/validation.sh ${args.version}
                """
 }
