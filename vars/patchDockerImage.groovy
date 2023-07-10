8d51e6b5eac1b3f029011d31c1fa3f32262e0ab5
Divya Madala <divyaasm@amazon.com>
2023-07-10 10:16:11 -0700

    Add variables
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- vars/patchDockerImage.groovy
+++ vars/patchDockerImage.groovy
@@ -9,9 +9,12 @@
 
 void call(Map args = [:]) {
     String docker_image = "opensearchproject/${args.product}:${args.version}"
+    String build_time = ""
+    String build_number = ""
     sh"""
     docker pull ${docker_image}
-    docker inspect --format '{{ index .Config.Labels "org.label-schema.build-date"}}' ${docker_image}
+    $build_time = docker inspect --format '{{ index .Config.Labels "org.label-schema.build-date"}}' ${docker_image}
+    $build_number = docker inspect --format '{{ index .Config.Labels "org.label-schema.description"}}' ${docker_image}
     """
-    println("docker image sucessfully pulled and inspected, exit 1")
+    println("docker image sucessfully pulled and inspected, exit 1 ${build_time} ${build_number}")
 }
