2a5ce1b2ce31d898deeecdb1fcdc84ad1f391fb7
Divya Madala <divyaasm@amazon.com>
2023-07-10 13:14:04 -0700

    Add changes
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- vars/patchDockerImage.groovy
+++ vars/patchDockerImage.groovy
@@ -17,6 +17,9 @@ void call(Map args = [:]) {
 
     sh"""
     #!/bin/bash
+    set -e
+    set +x
+
     docker pull ${docker_image}
     """
     sh"""
@@ -24,7 +27,7 @@ void call(Map args = [:]) {
     export build_number=`docker inspect --format '{{ index .Config.Labels "org.label-schema.description"}}' ${docker_image}`
     """
 
-    $build_number = sh""" docker inspect --format '{{ index .Config.Labels "org.label-schema.description"}}' ${docker_image} """
+    build_number = sh "`docker inspect --format '{{ index .Config.Labels "org.label-schema.description"}}' ${docker_image}`"
     println("${build_number}")
 /*
     staging_image = ${staging_image} + "${build_number}"
