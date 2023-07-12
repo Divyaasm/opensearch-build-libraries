49494b828ce4d4d18e1892c53a3a26a519813de9
Divya Madala <divyaasm@amazon.com>
2023-07-12 16:10:16 -0700

    Add changes
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- vars/patchDockerImage.groovy
+++ vars/patchDockerImage.groovy
@@ -49,7 +49,7 @@ void call(Map args = [:]) {
     staging_digest=`docker inspect ${staging_image} | jq -r '.[0].RepoDigests[0]' | cut -d'@' -f2`
 
 
-    if [ "$prod_digest" -eq "$staging_digest" ]
+    if [ "$prod_digest" == "$staging_digest" ]
     then
         echo "True"
     else
