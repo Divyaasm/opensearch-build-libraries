e1258d551606bbc3018cb8b82547ea50ef305d05
Divya Madala <divyaasm@amazon.com>
2024-02-21 15:17:18 -0800

    change parameter names in groovy library
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- vars/validateArtifacts.groovy
+++ vars/validateArtifacts.groovy
@@ -16,15 +16,20 @@ void call(Map args = [:]) {
         println("Validation.sh script not found in the workspace: ${WORKSPACE}, exit 1")
         System.exit(1)
     }
-    else {
-        println("Script file exists")}
 
     sh([
-        '${WORKSPACE}/validation.sh',
+        './validation.sh',
         args.version ? "--version ${args.version}" : null,
-        args.distribution ? "-d ${args.distribution}" : null,
-        args.platform ? "-p ${args.platform}" : null,
-        args.arch ? "-a ${args.arch}" : null,
-        args.projects ? "--projects ${args.projects}" : null
+        args.file_path ? "--file-path ${args.file_path}" : null,
+        args.distribution ? "--distribution ${args.distribution}" : null,
+        args.platform ? "--platform ${args.platform}" : null,
+        args.arch ? "--arch ${args.arch}" : null,
+        args.projects ? "--projects ${args.projects}" : null,
+        args.docker_source ? "--docker-source ${args.docker_source}" : null,
+        args.os_build_number ? "--os-build-number ${args.os_build_number}" : null,
+        args.osd_build_number ? "--osd-build-number ${args.osd_build_number}" : null,
+        args.artifact_type ? "--artifact-type ${args.artifact_type}" : null,
+        args.allow_http ? '--allow_http' : null,
+        args.docker_args ? "${args.artifact_type}" : null,
     ].join(' '))
 }
