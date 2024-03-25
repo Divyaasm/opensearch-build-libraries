b72c463098de2374f82717fb15e65386513d8ea2
Divya Madala <divyaasm@amazon.com>
2024-03-25 15:08:19 -0700

    test jenkins changes
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- tests/jenkins/TestRunBenchmarkTestScriptWithEndpoint.groovy
+++ tests/jenkins/TestRunBenchmarkTestScriptWithEndpoint.groovy
@@ -16,15 +16,15 @@ import static org.hamcrest.CoreMatchers.equalTo
 import static org.hamcrest.CoreMatchers.hasItem
 import static org.hamcrest.MatcherAssert.assertThat
 
-class TestRunBenchmarkTestScriptNoManifest extends BuildPipelineTest {
+class TestRunBenchmarkTestScriptWithEndpoint extends BuildPipelineTest {
 
     @Before
     void setUp() {
         this.registerLibTester(new RunBenchmarkTestScriptLibTester(
                 '',
-                'https://www.exmaple.com/example.tar.gz',
-                '3.0.0',
                 '',
+                '',
+                'opensearch-ABCxdfdfhyfk.com',
                 'true',
                 'nyc_taxis',
                 'true',
@@ -49,23 +49,20 @@ class TestRunBenchmarkTestScriptNoManifest extends BuildPipelineTest {
 
     @Test
     public void testRunBenchmarkTestScript_PipelineSingleNode() {
-        super.testPipeline("tests/jenkins/jobs/BenchmarkTestNoManifest_Jenkinsfile")
+        super.testPipeline("tests/jenkins/jobs/BenchmarkTestWithEndpoint_Jenkinsfile")
     }
 
     @Test
     void testRunBenchmarkTestScript_verifyScriptExecutionsNoManifest() {
-        runScript("tests/jenkins/jobs/BenchmarkTestNoManifest_Jenkinsfile")
+        runScript("tests/jenkins/jobs/BenchmarkTestWithEndpoint_Jenkinsfile")
 
         def testScriptCommands = getCommandExecutions('sh', './test.sh').findAll {
             shCommand -> shCommand.contains('./test.sh')
         }
 
-        assertThat(testScriptCommands.size(), equalTo(2))
+        assertThat(testScriptCommands.size(), equalTo(1))
         assertThat(testScriptCommands, hasItem(
-                "./test.sh benchmark-test  --distribution-url https://www.exmaple.com/example.tar.gz --distribution-version 3.0.0  --config /tmp/workspace/config.yml --workload nyc_taxis --benchmark-config /tmp/workspace/benchmark.ini --user-tag security-enabled:true  --single-node  --use-50-percent-heap   --capture-segment-replication-stat --suffix 307-secure      --data-instance-type r5.8xlarge  --test-procedure custom-test-procedure   --additional-config cluster.indices.replication.strategy:SEGMENT --data-node-storage 200 --ml-node-storage 200  ".toString()
-        ))
-        assertThat(testScriptCommands, hasItem(
-                "./test.sh benchmark-test  --distribution-url https://www.exmaple.com/example.tar.gz --distribution-version 3.0.0  --config /tmp/workspace/config.yml --workload nyc_taxis --benchmark-config /tmp/workspace/benchmark.ini --user-tag security-enabled:false --without-security --single-node  --use-50-percent-heap   --capture-segment-replication-stat --suffix 307      --data-instance-type r5.8xlarge  --test-procedure custom-test-procedure   --additional-config cluster.indices.replication.strategy:SEGMENT --data-node-storage 200 --ml-node-storage 200  ".toString()
+                "./test.sh benchmark-test    --cluster-endpoint opensearch-ABCxdfdfhyfk.com  --workload nyc_taxis --benchmark-config /tmp/workspace/benchmark.ini --user-tag security-enabled:true  --single-node  --use-50-percent-heap   --capture-segment-replication-stat --suffix 307-secure      --data-instance-type r5.8xlarge  --test-procedure custom-test-procedure   --additional-config cluster.indices.replication.strategy:SEGMENT --data-node-storage 200 --ml-node-storage 200  "
         ))
     }
 
