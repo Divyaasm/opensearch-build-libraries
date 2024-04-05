5e17501bcc9f5d2041e1b8db991561ab63be38b4
Divya Madala <divyaasm@amazon.com>
2024-04-05 16:31:18 -0700

    Add library changes
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- vars/runBenchmarkTestScript.groovy
+++ vars/runBenchmarkTestScript.groovy
@@ -109,7 +109,9 @@ void call(Map args = [:]) {
         isNullOrEmpty(args.telemetryParams) ? "" : "--telemetry-params '${args.telemetryParams}'"
     ].join(' ').trim()
 
-    sh """set +x && ${command}"""
+    sh """set +x
+          ${command}"""
+
 
 }
 
