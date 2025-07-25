73e3bee78102f162234f4a975466535b3672d878
Divya Madala <divyaasm@amazon.com>
2025-07-25 12:42:23 -0700

    Remove changes
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- src/utils/OpenSearchMetricsQuery.groovy
+++ src/utils/OpenSearchMetricsQuery.groovy
@@ -35,6 +35,7 @@ class OpenSearchMetricsQuery {
             script: """
             set -e
             set +x
+            echo "Current shell: \$0"
             MONTH_YEAR=\$(date +"%m-%Y")
             curl -s -XGET "${metricsUrl}/${name}*/_search" --aws-sigv4 "aws:amz:us-east-1:es" --user "${awsAccessKey}:${awsSecretKey}" -H "x-amz-security-token:${awsSessionToken}" -H 'Content-Type: application/json' -d "${query}" | jq '.'
         """,
