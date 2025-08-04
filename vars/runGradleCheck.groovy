33fdd78844c1ee6bd96df0d4cd5277c83ff47943
Divya Madala <divyaasm@amazon.com>
2025-08-04 16:44:24 -0700

    Updtae gradle command
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- vars/runGradleCheck.groovy
+++ vars/runGradleCheck.groovy
@@ -78,7 +78,7 @@ void call(Map args = [:]) {
 
                 echo "Start gradlecheck"
                 GRADLE_CHECK_STATUS=0
-                ./gradlew clean && ./gradlew ':rest-api-spec:yamlRestTest' --tests 'org.opensearch.test.rest.ClientYamlTestSuiteIT' -Dtests.coverage=true ${bwc_checkout_align_param} --no-daemon --no-scan || GRADLE_CHECK_STATUS=1
+                ./gradlew clean && ./gradlew ':plugins:telemetry-otel:internalClusterTest' --tests 'org.opensearch.telemetry.metrics.TelemetryMetricsEnabledSanityIT.testGaugeWithValueAndTagSupplier' -Dtests.coverage=true ${bwc_checkout_align_param} --no-daemon --no-scan || GRADLE_CHECK_STATUS=1
 
                 if [ "\$GRADLE_CHECK_STATUS" != 0 ]; then
                     echo Gradle Check Failed!
