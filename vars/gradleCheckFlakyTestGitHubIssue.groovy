32345d4d11d439bcac7366241770851f9c16ae26
Divya Madala <divyaasm@amazon.com>
2025-08-08 10:34:16 -0700

    Create markdoen table
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- vars/gradleCheckFlakyTestGitHubIssue.groovy
+++ vars/gradleCheckFlakyTestGitHubIssue.groovy
@@ -23,30 +23,12 @@ import gradlecheck.MarkdownComparator
 void call(Map args = [:]) {
     label = args.label ?: 'autocut,>test-failure,flaky-test'
     try {
-        withCredentials([usernamePassword(credentialsId: 'jenkins-github-bot-token', passwordVariable: 'GITHUB_TOKEN', usernameVariable: 'GITHUB_USER')]) {
-            def openIssue = sh(
-                    script: "gh issue list --repo ${args.repoUrl} -S \"${args.issueTitle} in:title\" --json number --jq '.[0].number'",
-                    returnStdout: true
-            ).trim()
-
-
-            def closedIssue = sh(
-                    script: "gh issue list --repo ${args.repoUrl} -S \"${args.issueTitle} in:title is:closed\" --json number --jq '.[0].number'",
-                    returnStdout: true
-            ).trim()
-
-            if (openIssue) {
-                println('Issue already exists, editing the issue body')
-                sh(
-                        script: "gh issue edit ${openIssue} --repo ${args.repoUrl} --body-file \"${args.issueBodyFile}\"",
-                        returnStdout: true
-                )
-            }
-            else if (!openIssue && closedIssue) {
                 def existingIssueBody = sh(
-                        script: "gh issue list --repo ${args.repoUrl} -S \"${args.issueTitle} in:title is:closed\"  --json body --jq '.[0].body'",
+                        script: "gh issue list --repo https://github.com/opensearch-project/opensearch-build-libraries -S \"[Bug]: Formatting issue with documentation issue comments in:title is:open\" --json number --jq '.[0].number'",
                         returnStdout: true
                 ).trim()
+
+
                 def existingTable = new ParseMarkDownTable(existingIssueBody).parseMarkdownTableRows()
                 def markdownTable = new ParseMarkDownTable(readFile(args.issueBodyFile)).parseMarkdownTableRows()
                 def differences = new MarkdownComparator(markdownTable, existingTable).markdownComparison()
@@ -60,24 +42,9 @@ void call(Map args = [:]) {
                                 "Build Details: ${diffRow['Build Details']}, " +
                                 "Test Name: ${diffRow['Test Name']}"
                     }
-                    sh(
-                            script: "gh issue reopen --repo ${args.repoUrl} ${closedIssue}",
-                            returnStdout: true
-                    )
-                    sh(
-                            script: "gh issue edit ${closedIssue} --repo ${args.repoUrl} --body-file \"${args.issueBodyFile}\"",
-                            returnStdout: true
-                    )
                 }
-            }
-            else {
-                println("Creating new issue")
-                sh(
-                        script: "gh issue create --title \"${args.issueTitle}\" --body-file \"${args.issueBodyFile}\" --label \"${label}\" --label \"untriaged\" --repo ${args.repoUrl}",
-                        returnStdout: true
-                )
-            }
-        }
+
+
     } catch (Exception ex) {
         error("Unable to create GitHub issue for ${args.repoUrl}", ex.getMessage())
     }
