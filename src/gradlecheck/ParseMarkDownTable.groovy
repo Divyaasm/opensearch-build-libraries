3c8cc96c2744b4d82dec075489770a43de0b308a
Divya Madala <divyaasm@amazon.com>
2025-08-11 14:44:37 -0700

    test issue update
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- src/gradlecheck/ParseMarkDownTable.groovy
+++ src/gradlecheck/ParseMarkDownTable.groovy
@@ -17,7 +17,7 @@ class ParseMarkDownTable {
     }
 
     def parseMarkdownTableRows() {
-        println "mark"
+        print "mark"
         def rows = markdown.split("\\|\\s*\\n")
         rows = rows[2..-2] // Skipping headers and footer
         return rows.collect { row ->
