73c188c0aecf28678e0d4546264ee3901b9c4766
Divya Madala <divyaasm@amazon.com>
2025-08-21 10:51:47 -0700

    Add server parameter
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- vars/runGradleCheck.groovy
+++ vars/runGradleCheck.groovy
@@ -12,7 +12,7 @@ void call(Map args = [:]) {
     def git_reference = args.gitReference ?: 'null'
     def bwc_checkout_align = args.bwcCheckoutAlign ?: 'false'
     def bwc_checkout_align_param = ''
-    def command =  args.serverModule ? ":server:check " : "check -x :server:check -x :server:test"
+    def command =  args.serverModule ? ":server:check" : "check -x :server:check -x :server:test"
 
     println("Git Repo: ${git_repo_url}")
     println("Git Reference: ${git_reference}")
