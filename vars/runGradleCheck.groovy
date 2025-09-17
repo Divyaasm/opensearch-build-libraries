793c8839b02c6d23f72292ebf60f80f7b5ce731c
Divya Madala <divyaasm@amazon.com>
2025-09-17 14:31:45 -0700

    Add gradle check command changes
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- vars/runGradleCheck.groovy
+++ vars/runGradleCheck.groovy
@@ -12,7 +12,7 @@ void call(Map args = [:]) {
     def git_reference = args.gitReference ?: 'null'
     def bwc_checkout_align = args.bwcCheckoutAlign ?: 'false'
     def bwc_checkout_align_param = ''
-    def command =  ":server:check"
+    def command =  args.serverModule ? ":server:check :server:jacocoTestReport" : "check -x :server:check"
 
     println("Git Repo: ${git_repo_url}")
     println("Git Reference: ${git_reference}")
@@ -79,7 +79,7 @@ void call(Map args = [:]) {
 
                 echo "Start gradlecheck"
                 GRADLE_CHECK_STATUS=0
-                ./gradlew clean && ./gradlew :server:check :server:jacocoTestReport -Dtests.coverage=true ${bwc_checkout_align_param} --no-daemon --no-scan || GRADLE_CHECK_STATUS=1
+                ./gradlew clean && ./gradlew ${command} -Dtests.coverage=true ${bwc_checkout_align_param} --no-daemon --no-scan || GRADLE_CHECK_STATUS=1
                 if [ "\$GRADLE_CHECK_STATUS" != 0 ]; then
                     echo Gradle Check Failed!
                     exit 1
